6f93ef2ab0ba1fc0f02bcae65af7b805cf98da46
SyedMuhgisHussain <syedmughis4541@gmail.com>
2023-07-12 17:29:17 +0500

    search bar added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,9 +2,8 @@ import 'package:flutter/material.dart';
 import '../services/weather_api_client.dart';
 
 import '../model/weather.dart';
-import '../widgets/current_weather_widget.dart';
-// ignore: depend_on_referenced_packages
 import '../widgets/additional_information_widget.dart';
+import '../widgets/current_weather_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -14,11 +13,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController searchController = TextEditingController();
   WeatherApiClient client = WeatherApiClient();
   Weather? data;
 
   Future<void> getData() async {
-    data = (await client.getData('Faisalabad'));
+    data = (await client.getData(searchController.text));
   }
 
   @override
@@ -39,27 +39,61 @@ class _HomeScreenState extends State<HomeScreen> {
           future: getData(),
           builder: (context, snapshot) {
             if (snapshot.connectionState == ConnectionState.done) {
-              return Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  currentWeatherWidget(
-                      Icons.wb_sunny_rounded,
-                      "${(data!.temp! - 273.15).toStringAsFixed(1)} ⁰C",
-                      "${data!.cityName}"),
-                  const SizedBox(
-                    height: 40.0,
-                  ),
-                  const Text(
-                    'Additional Information',
-                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
-                  ),
-                  const Divider(),
-                  const SizedBox(
-                    height: 10.0,
-                  ),
-                  addtionalInformation('${data!.wind}', "${data!.pressure}",
-                      "${data!.humidity}", "${data!.feels_like}"),
-                ],
+              return SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                          border: Border.all(color: Colors.black),
+                          borderRadius:
+                              const BorderRadius.all(Radius.circular(23))),
+                      height: 50,
+                      width: 350,
+                      child: Row(
+                        children: [
+                          IconButton(
+                              onPressed: () {
+                                if (searchController.text.isNotEmpty) {
+                                  setState(() {
+                                    getData();
+                                  });
+                                }
+                              },
+                              icon: const Icon(Icons.search)),
+                          Expanded(
+                              child: TextField(
+                            controller: searchController,
+                            decoration: const InputDecoration(
+                                border: InputBorder.none,
+                                hintText: 'Search City',
+                                focusedBorder: InputBorder.none),
+                            keyboardType: TextInputType.text,
+                          )),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    currentWeatherWidget(Icons.wb_sunny_rounded,
+                        "${data!.temp!} ⁰C", "${data!.cityName}"),
+                    const SizedBox(
+                      height: 40.0,
+                    ),
+                    const Text(
+                      'Additional Information',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                    ),
+                    const Divider(),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    addtionalInformation('${data!.wind}', "${data!.pressure}",
+                        "${data!.humidity}", "${data!.feels_like}"),
+                  ],
+                ),
               );
             } else if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(
